450c5fe77a15fc427f14e3ded2a6da33771880ed
Daniel Bluhm <dbluhm@pm.me>
2023-11-06 13:59:17 -0500

    feat: basic configuration
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,43 @@
-console.log("Hello, TypeScript Node server!");
+import {
+  InitConfig,
+  Agent,
+  KeyDerivationMethod,
+  ConsoleLogger,
+  LogLevel,
+  HttpOutboundTransport,
+} from "@aries-framework/core";
+import { agentDependencies, HttpInboundTransport } from "@aries-framework/node";
+import { AskarModule } from "@aries-framework/askar";
+import { ariesAskar } from "@hyperledger/aries-askar-nodejs";
+
+const key = ariesAskar.storeGenerateRawKey({});
+
+const config: InitConfig = {
+  label: "docs-agent-nodejs",
+  logger: new ConsoleLogger(LogLevel.debug),
+  walletConfig: {
+    id: "wallet-id",
+    key: key,
+    keyDerivationMethod: KeyDerivationMethod.Raw,
+    storage: {
+      type: "sqlite",
+      inMemory: true,
+    },
+  },
+};
+
+const agent = new Agent({
+  config,
+  dependencies: agentDependencies,
+  modules: {
+    // Register the Askar module on the agent
+    askar: new AskarModule({
+      ariesAskar,
+    }),
+  },
+});
+
+agent.registerOutboundTransport(new HttpOutboundTransport());
+agent.registerInboundTransport(new HttpInboundTransport({ port: 3001 }));
+
+agent.initialize();
